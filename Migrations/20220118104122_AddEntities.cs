c73103c046faae51acc683b2b2d65ffc94b41f4a
Benoit Gerard <benoit.gerard@tackops.com>
2022-01-19 18:47:40 +0100

    Initial commit (cannot reproduce)

---
+++ Migrations/20220118104122_AddEntities.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ReproEfBatch.Migrations
+{
+    public partial class AddEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Entities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entities", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Entities");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+Console.WriteLine("ReproEfBatch started");
+using (var context = new Context())
+{
+    context.Database.Migrate();
+
+    context.DeleteRangeAsync<Entity>();
+
+    Debug.Assert(context.Entities.Count() == 0);
+    Console.WriteLine("ReproEfBatch entities emptied");
+
+    context.Entities.Add(new Entity());
+    context.Entities.Add(new Entity());
+    context.Entities.Add(new Entity());
+    context.SaveChanges();
+
+    Debug.Assert(context.Entities.Count() == 3);
+    Console.WriteLine("ReproEfBatch entities added");
+
+    var first = context.Entities.First();
+    context.DeleteRangeAsync<Entity>(e => e.Id != first.Id);
+
+    Debug.Assert(context.Entities.Count() == 1);
+    Console.WriteLine("ReproEfBatch entities deleted");
+
+    Console.ReadKey();
+}
+
+class Entity
+{
+    public int Id { get; set; }
+}
+
+class Context : DbContext
+{
+    public DbSet<Entity> Entities => Set<Entity>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.LogTo(Console.WriteLine);
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=ReproEfBatch;Username=postgres;Password=admin");
+        optionsBuilder.UseBatchEF_Npgsql();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Entity>().Property(e => e.Id).UseIdentityAlwaysColumn();
+    }
+}
